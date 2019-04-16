df7223d547e673f40429961b5419adc7eac1ed4a
thewolfx41 <mjorgeg@uda.ad>
2019-04-16 16:31:29 +0200

    cryptor support & fixes

--- main.py
+++ main.py
@@ -2,41 +2,58 @@
 
 import boto3
 import sys
+import datetime
 from urllib.request import urlopen
 from time import sleep
+from cryptor import AESCipher
+
+
+banner = '''  ___               ___  _  _ ___      ___      _____ 
+ |   \ _  _ _ _ ___|   \| \| / __|___ /_\ \    / / __|
+ | |) | || | ' \___| |) | .` \__ \___/ _ \ \/\/ /\__ \\
+ |___/ \_, |_||_|  |___/|_|\_|___/  /_/ \_\_/\_/ |___/
+       |__/                                           
+'''
+
+global KEY_ID
+global KEY_ACCESS
+
+KEY_ID = b'z3+XCgcbyXfzE6c9avvfKYPyaayAkQjzJ7uEuuMALy2EKeMMxjtttUMuOaDGQUcf'
+KEY_ACCESS = b'l0Q4/U1hXtf1dknVhLXjvMgPdxNeQutYDv2Ta4e/yAPe59cTRB5nAkH38mgo7atpy3QEHP1d2Dqz8IDHjw4XNZhXMpyd8rqHDENyrrSzIgE='
 
 
 def check():
-    global KEY_ID
-    global KEY_ACCESS
     global REGION
     global ZONE_ID
     global FQDN
+    global MY_IP
 
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 4:
         print("[!] Error on arguments.")
-        print("Example: {} key_id key_access_key zone_id fqdn".format(sys.argv[0]))
+        print("Example: {} password zone_id fqdn".format(sys.argv[0]))
         sys.exit(0)
     else:
-        KEY_ID = str(sys.argv[1])
-        KEY_ACCESS = str(sys.argv[2])
-        ZONE_ID = str(sys.argv[3])
-        FQDN = str(sys.argv[4])
+        ZONE_ID = str(sys.argv[2])
+        FQDN = str(sys.argv[3])
         REGION = 'us-east-1'
+        try:
+            MY_IP = check_ip()
+        except:
+            pass
 
 
 def init_service():
     global KEY_ID
     global KEY_ACCESS
     global REGION
-    global MY_IP
+
+    crypto = AESCipher(str(sys.argv[1]))
 
     try:
         client = boto3.client('route53',
-                              aws_access_key_id=KEY_ID,
-                              aws_secret_access_key=KEY_ACCESS,
+                              aws_access_key_id=str(crypto.decrypt(KEY_ID)),
+                              aws_secret_access_key=str(crypto.decrypt(KEY_ACCESS)),
                               region_name=REGION)
-        MY_IP = check_ip()
         return client
     except:
         print("[!] Error contacting with aws API!")
@@ -74,7 +91,7 @@ def set_dynamic_ip(client, ip, zone_id, fqdn):
                 ]
             }
         )
-        print("[*] Record modified!")
+        print("[*] Record modified! " + str(datetime.datetime.now()) + "\n")
     except:
         print("[!] Error modifying dns record.")
         pass
@@ -86,14 +103,19 @@ def main():
     global FQDN
 
     check()
-    api_client = init_service()
-    
-    while True:
-        if MY_IP != check_ip():
-            MY_IP = check_ip()
-            set_dynamic_ip(api_client, MY_IP, ZONE_ID, FQDN)
-        print(MY_IP)
-        sleep(5)
+    print(banner + "\n")
+
+    try:
+        while True:
+            if MY_IP != check_ip():
+                api_client = init_service()
+                MY_IP = check_ip()
+                set_dynamic_ip(api_client, MY_IP, ZONE_ID, FQDN)
+                del api_client
+            print("Your IP is " + MY_IP + " at " + str(datetime.datetime.now()))
+            sleep(120)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 
 if __name__ == '__main__':
